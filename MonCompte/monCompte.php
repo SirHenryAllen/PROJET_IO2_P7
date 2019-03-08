e7abb501d449749d769c8bd1809337b698e9fd4b
PullThePin <nadim.hamimid@gmail.com>
2019-03-08 12:09:25 +0100

    l

---
+++ MonCompte/monCompte.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Se connecter aux 80's</title>
+		<link rel="stylesheet" href="styleMonCompte.css">
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li class="list-home"><a href="Index.html"><img id="home" src="Images_CSS/CSS.png"></a>
+				</li>	
+				</li>
+				<li class="list-Jeux"><a href="#">Jeux</a>
+					<ul class="sousliste">
+						<li><a href="Jeux/Jeux.html">Toute la selection</a></li>
+						<li><a href="#">Recenser</a></li>
+						<li><a href="#">Les Mieux notés</a></li>
+					</ul>
+				</li>
+				<li class="list-Actus"><a href="Actus.html">Actus</a>
+					<ul class="sousliste">
+						<li><a href="#">Toute les news</a></li>
+						<li><a href="#">Articles et Chroniques récentes</a></li>
+					</ul>
+				</li>
+				<li class="list-Critiques"><a href="Critiques.html">Critiques</a>
+					<ul class="sousliste">
+						<li><a href="#">Toutes les critiques</a></li>
+						<li><a href="#">Critiques récentes</a></li>
+					</ul>
+				</li>
+				<li class="list-Forum"><a href="Forum.html">Forum</a>
+				</li>
+				<li class="list-Communauté"><a href="Communauté.html">Communauté</a>
+					<ul class="sousliste">
+						<li><a href="#">Vos Articles</a></li>
+						<li><a href="#">Vos Critiques</a></li>
+					</ul>
+				</li>
+				<li class="list-Moncompte"><a href="Moncompte.html"><img id="pdp" src="Images_CSS/pdp.png">Mon Compte</a>
+				</li>		
+			</ul>
+		</nav>
+	</body>
+
+</html>
