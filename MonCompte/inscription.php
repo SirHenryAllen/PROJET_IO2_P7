a1221179b7bc54b0a13d41df4ee376c52179a4e2
Sir Henry Allen <matt.lefranc@gmail.com>
2019-05-03 16:56:34 +0200

    modifié :         inscription.php 	modifié :         monCompte.php 	modifié :         profil.php

--- MonCompte/inscription.php
+++ MonCompte/inscription.php
@@ -60,7 +60,7 @@
 				</li>
 				<li class="list-Jeux"><a href="#">Jeux</a>
 					<ul class="sousliste">
-						<li><a href="../Jeux/Selection/Selection.html">Toute la selection</a></li>
+						<li><a href="../Jeux/Selection/Selection.php">Toute la selection</a></li>
 						<li><a href="../Jeux/Recenser/Recenser.php">Recenser</a></li>
 					</ul>
 				</li>
@@ -70,7 +70,7 @@
 						<li><a href="#">Articles et Chroniques récentes</a></li>
 					</ul>
 				</li>
-				<li class="list-Critiques"><a href="../Critiques/critiques.html">Critiques</a>
+				<li class="list-Critiques"><a href="../Critiques/critiques.php">Critiques</a>
 				</li>
 				<li class="list-Forum"><a href="Forum.html">Forum</a>
 				</li>
